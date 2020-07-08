4501bc234cc7bf30cbbecaed11e34edae767f77d
gzyinkaixuan <gzyinkaixuan@corp.netease.com>
2020-07-08 15:28:45 +0800

    debug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 VERSION = 0.1
 
 setup(
-  name="mytools",
+  name="ykxtools",
   version=VERSION,
   packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
   install_requires=[
@@ -11,9 +11,12 @@ setup(
     "psutil",
   ],
   entry_points={
+
     "tools_scripts": [
       "split_str = mytools.mytools:split_str",
       "read_json_file = mytools.mytools.read_json_file",
+
     ],
   },
+
 )
